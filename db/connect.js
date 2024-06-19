c947ad5cbc4b0e4f5b7e151c4e33767739b8aae5
Avas <avas.bajracharya@renegadeinsurance.com>
2024-06-19 12:57:34 +0545

    feat: added mysql2 dialect

--- db/connect.js
+++ db/connect.js
@@ -5,6 +5,7 @@ import { ENV } from "../const/env.js";
 const sequelize = new Sequelize(ENV.DATABASE_NAME, ENV.USERNAME, ENV.PASSWORD, {
   dialect: "mysql",
   host: ENV.HOST_NAME,
+  dialectModule: "mysql2",
 });
 
 export default sequelize;
